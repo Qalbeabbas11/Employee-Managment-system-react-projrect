dadd69bd6b4425a4128b5fee1d854fe78c1f349d
Qalb e Abbas <qalbshah10@gmail.com>
2025-08-29 01:43:56 +0500

    Updated Login.jsx favicon path

--- src/Components/Auth/Login.jsx
+++ src/Components/Auth/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import favicon from "./assets/images/favicon.png";
+
 
 const Login = ({handleLogin}) => {
   
@@ -23,7 +23,7 @@ const Login = ({handleLogin}) => {
         <div className=" p-10 rounded-xl bg-emerald-100">
           <div className="head flex justify-center flex-col items-center mb-3 gap-3">
             <img
-              src="{favicon}"
+              src="public/images/favicon.png"
               className="w-32 h-32 object-cover rounded-md "
               alt=""
             />
